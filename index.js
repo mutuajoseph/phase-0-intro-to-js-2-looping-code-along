59775e55a247f272722e2e11627b446ac1f870c8
mutuajoseph <wambuajoseph756@gmail.com>
2022-03-17 13:46:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let messageArray = [];
+    let message = "";
+    for (let i = 0; i < names.length; i++) {
+        message = `Thank you, ${names[i]}, for the wonderful surprise visit!`;
+        console.log(message);
+        messageArray.push(message);
+        console.log(messageArray);
+
+    };
+    console.log("going out loop")
+
+    console.log(messageArray);
+    return messageArray;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
+countDown(10);
